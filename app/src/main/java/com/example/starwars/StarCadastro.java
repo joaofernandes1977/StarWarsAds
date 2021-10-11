05dbcadcfbc6cf8e08f6dafd0e1c2ed5df69117a
Joao Fernandes <joao.fernandez.hl@gmail.com>
2021-10-11 00:40:30 -0300

    start

---
+++ app/src/main/java/com/example/starwars/StarCadastro.java
@@ -0,0 +1,15 @@
+package com.example.starwars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StarCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_cadastro);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/example/starwars/StarLogin.java
+++ app/src/main/java/com/example/starwars/StarLogin.java
@@ -2,14 +2,34 @@ package com.example.starwars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StarLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_login);
+
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cadastro = new Intent(StarLogin.this, StarCadastro.class);
+                      startActivity(cadastro);
+            }
+        });
+
+
+    }
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.cad_nova_conta);
     }
 }
